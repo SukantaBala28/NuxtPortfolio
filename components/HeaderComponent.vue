5557b3ad8520ed5506aa28fb122a5191a363dd74
Sazzad Hossain <31382253+sazzad005@users.noreply.github.com>
2017-12-24 01:06:45 +0600

    Add files via upload
    small canges

--- components/HeaderComponent.vue
+++ components/HeaderComponent.vue
@@ -1,40 +1,34 @@
 <template>
-  <div class="main">
-    <div class="header">
-      <div class="container">
-        <nav class="navbar navbar-expand-lg bg-faded fixed-top" style="background-color: #727F80;border-bottom: 2px solid #007BFF;" :class="{showBar: scrollPosition < 20, hideBar: scrollPosition > 20}">
-          <a class="navbar-brand" href="#">SUKANTA BALA</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon">
-              <i class="fa fa-bars" aria-hidden="true"></i>
-            </span>
-          </button>
-          <div class="collapse navbar-collapse navbar-item" id="navbarTogglerDemo01">
-            <ul class="navbar-nav ml-auto">
-              <li class="nav-item">
-                <a class="nav-link" v-scroll-to="'#about'">ABOUT ME</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" v-scroll-to="'#services'">SERVICES</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" v-scroll-to="'#portfolio'">PORTFOLIO</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" v-scroll-to="'#team'">OUR TEAM</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" v-scroll-to="'#feedback'">FEEDBACK</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" v-scroll-to="'#contact'">CONTACT ME</a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </div>
+  <nav class="navbar navbar-expand-lg bg-faded fixed-top" style="background-color: #727F80;border-bottom: 2px solid #007BFF;" :class="{showBar: scrollPosition < 20, hideBar: scrollPosition > 20}">
+    <a class="navbar-brand" href="#">SUKANTA BALA</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon">
+        <i class="fa fa-bars" aria-hidden="true"></i>
+      </span>
+    </button>
+    <div class="collapse navbar-collapse navbar-item" id="navbarTogglerDemo01">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" v-scroll-to="'#about'">ABOUT ME</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" v-scroll-to="'#services'">SERVICES</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" v-scroll-to="'#portfolio'">PORTFOLIO</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" v-scroll-to="'#team'">OUR TEAM</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" v-scroll-to="'#feedback'">FEEDBACK</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" v-scroll-to="'#contact'">CONTACT ME</a>
+        </li>
+      </ul>
     </div>
-  </div>
+  </nav>
 </template>
 
 <script>
